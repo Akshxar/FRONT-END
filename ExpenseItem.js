087334b6591145a8c300664b31f488581ad1af86
Akshxar <144107944+Akshxar@users.noreply.github.com>
2023-12-10 12:45:38 +0530

    change Expense $100

--- ExpenseItem.js
+++ ExpenseItem.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ExpenseItem.css'
 import ExpenseDate from './ExpenseDate.js';
 import ExpenseDetails from './ExpenseDetails';
@@ -10,19 +10,25 @@ const ExpenseItem=(props)=>
         event.target.parentElement.remove();
     }
     
-    let title=props.title
+    const[title,setTitle] =useState(props.title)
     const TitleHandler = ()=>{
-        alert("changeTitle")
+        setTitle("updated")
         
+    }
+
+    const[amount,setAmount] =useState(props.amount)
+    const amountHandler =()=>{
+        setAmount("100")
     }
 
     return(
         < div className='expense_item'> 
              
         <ExpenseDate date={props.date}></ExpenseDate>
-        <ExpenseDetails title={title} amount={props.amount} />
+        <ExpenseDetails title={title} amount={amount} />
         <button onClick={TitleHandler}>Change Title </button>
         <button onClick={DeleteHandler}>Delete Expense </button>
+        <button onClick={amountHandler}>Amount Expense</button>
         
         </div>     
     )
